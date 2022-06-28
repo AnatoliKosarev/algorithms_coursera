6359ce999dd6a7a6977e8d4d875f257c066af7b1
AnatoliKosarev <anatoli.kosarev@gmail.com>
2022-06-28 20:38:14 +0200

    Added count_inversion algorithm

---
+++ Part1/Week2/count_inversion_algo.py
@@ -0,0 +1,44 @@
+def count_inversion_algo(list_to_sort):
+    inv_count = 0
+    sorted_list = []
+
+    if len(list_to_sort) <= 1:
+        return {'sorted_list': list_to_sort}
+
+    med = len(list_to_sort) // 2
+
+    left = list_to_sort[:med]
+    right = list_to_sort[med:]
+
+    result_for_left = count_inversion_algo(left)
+    result_for_right = count_inversion_algo(right)
+    sorted_left = result_for_left.get('sorted_list', False)
+    sorted_right = result_for_right.get('sorted_list', False)
+    inv_count += (result_for_left.get('inv_count', False) + result_for_right.get('inv_count', False))
+
+    i = 0
+    j = 0
+
+    while i < len(sorted_left) and j < len(sorted_right):
+        if sorted_left[i] <= sorted_right[j]:
+            sorted_list.append(sorted_left[i])
+            i += 1
+        else:
+            inv_count += med - i
+            sorted_list.append(sorted_right[j])
+            j += 1
+
+    while i < len(sorted_left):
+        sorted_list.append(sorted_left[i])
+        i += 1
+
+    while j < len(sorted_right):
+        sorted_list.append(sorted_right[j])
+        j += 1
+
+    return {'sorted_list': sorted_list, 'inv_count': inv_count}
+
+
+test = [6, 5, 4, 3, 2, 1]
+result = count_inversion_algo(test)
+print(f'Number of inversions: {result.get("inv_count", False)}\nSorted list: {result.get("sorted_list", False)}')
